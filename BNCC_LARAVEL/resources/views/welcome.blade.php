821e0ce1626b92611f9693872f2481d77da1f449
Nicholas <nicho031995@gmail.com>
2023-11-29 16:21:49 +0700

    fisrt commit

---
+++ BNCC_LARAVEL/resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Pertemuan 1</title>
+    <style>
+        h1 {
+            color: blue;
+        }
+    </style>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- <h1>Pertemuan 1</h1>
+    <h2>Pertemuan 1</h2>
+    <h3>Pertemuan 1</h3>
+    <h4>Pertemuan 1</h4>
+    <h5>Pertemuan 1</h5>
+    <h6>Pertemuan 1</h6> -->
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore qui eligendi nemo, vero dolorem, ad temporibus, neque obcaecati necessitatibus ducimus tempore? Eligendi deserunt modi, a facilis officiis accusamus eos minima quaerat eius vero cupiditate adipisci assumenda illum minus quisquam tempora provident. Velit quod repellat est a tempore asperiores quia aspernatur?</p>
+
+    <a href="https://www.google.com">GOOGLE</a>
+
+    <img src="https://static-assets.tesla.com/configurator/compositor?context=design_studio_2?&bkba_opt=1&view=FRONT34&size=1400&model=mx&options=$MDLX,$MTX13,$PPSW,$WX00,$APBS,$CC01,$SC04,$CPF0,$IBE00,$ST0Y,$TW01&crop=1400,850,300,130&" alt="Image Mobil Tesla" 
+    style="width: 300px; height: 300px">
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestiae.</p>
+    <br><br><br>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, ab!</p>
+
+    <hr>
+
+    <form action="">
+        <label for="">Nama</label>
+        <input type="text">
+
+        <label for="">Email</label>
+        <input type="email">
+
+        <label for="">Message</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+
+        <button>Submit</button>
+    </form>
+
+    <br><br><br>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <td>Nicholas</td>
+            <td>12345</td>
+            <td>Computer Science</td>
+        </tr>
+    </table>
+
+    <div class="div1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio voluptatibus quis vel ut? Impedit nam tempora optio officia in esse perspiciatis, rerum iste? Deleniti sequi distinctio reprehenderit natus nam tenetur suscipit itaque consequuntur veniam fuga et corporis adipisci aspernatur hic laudantium perferendis, sit at omnis. Deserunt non nostrum est.</p>
+    </div>
+    <div id="div2">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, error voluptates possimus hic similique cum ex eius ipsa dolorum, molestiae quibusdam nisi sapiente. Illo soluta consequuntur officia fugit, architecto tempora possimus id autem saepe! Error aperiam enim fugiat esse provident, ullam iure fugit, dolorum explicabo earum quibusdam nulla voluptatum cupiditate!</p>
+    </div>
+</body>
+</html>
---
+++ Trial Class BNCC - BACK END/PDM_PYTHON.py
@@ -0,0 +1,72 @@
+# LATIHAN
+
+# print("hello world")
+# a = 20
+# print(a)
+
+# Int -> String
+# ab = "20"
+# print(ab)
+
+
+# b = "HALO"
+# print(b)
+
+# x = int(input("Masukkan umur: "))
+# print("Umur kamu adalah", x)
+
+
+# LOOPn
+# print("*"*20)
+# print("*\n"*20)
+
+# ipk = float(input("Masukkan nilai: "))
+# rint("Nilai", c)
+
+# nama = input("Masukkan nama: ")
+# print("Namaku adalah %s dengan IPK %s" %(nama, ipk))
+
+# Operator
+# a = int(input("Masukkan a: "))
+# b = int(input("Masukkan b: "))
+
+# print("Tambah:", a+b)
+# print("Kurang:", a-b)
+# print("Kali:", a*b)
+# print("Bagi:", a/b)
+# print("Pangkat:", a**b)
+# print("Modulo:", a%b)
+
+# print("AND:", a&b)
+# print("OR:", a|b)
+# print("Bitwise kanan:", a>>1)
+# print("Bitwise kiri:", a<<1)
+
+# print("a lebih kecil dari b:", a<b)
+# print("a lebih besar dari b:", a>b)
+
+
+# CALCULATOR
+# a = int(input("Masukkan a: "))
+# b = int(input("Masukkan b: "))
+
+# print("Tambah:", a+b)
+# print("Kurang:", a-b)
+# print("Kali:", a*b)
+# print("Bagi:", a/b)
+
+
+# IF ELSE
+# a = int(input("Masukkan a: "))
+# b = int(input("Masukkan b: "))
+# c = int(input("Masukkan c: "))
+
+# if(b > a):
+#     print("b lebih besar dari a")
+
+# elif(b > c):
+#     print("b lebih besar dari c")
+
+# else:
+#     print("b lebih kecil dari a dan c")
+#     print("Keren")
